fe85105c63e7ed1ddc5b771cf4a85576a220e775
Ganish112 <Zaza2234@proton.me>
2025-05-15 17:44:14 +0200

    Updated WhyChooseUsSection.jsx

--- src/components/Home/WhyChooseUsSection.jsx
+++ src/components/Home/WhyChooseUsSection.jsx
@@ -61,7 +61,7 @@ const WhyChooseUsSection = () => {
   };
 
   return (
-    <Section background="white">
+    <Section background="gray">
       <motion.div
         initial="hidden"
         whileInView="visible"
