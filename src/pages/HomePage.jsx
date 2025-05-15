2dfd9d2dc75f9248bd434e1564fbd23e409ffde5
Ganish112 <Zaza2234@proton.me>
2025-05-15 20:51:25 +0200

    Add Mockup Section to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,9 @@ import WhyChooseUsSection from '../components/Home/WhyChooseUsSection';
 import ClientsSection from '../components/Home/ClientsSection';
 import FaqSection from '../components/Home/FaqSection';
 import CtaSection from '../components/Home/CtaSection';
-import Responsive from '../components/Home/Responsive'
+import Responsive from '../components/Home/Responsive';
+import MockupSection from '../components/Home/MockupSection';
+
 const HomePage = () => {
   useEffect(() => {
     document.title = 'Obsidium | Web Development Company';
@@ -17,11 +19,11 @@ const HomePage = () => {
       <ServicesSection />
       <WhyChooseUsSection />
       <Responsive />
+      <MockupSection />
       <FaqSection />
       <CtaSection />
     </>
   );
 };
-//      <ClientsSection />
 
 export default HomePage;
